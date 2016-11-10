48e6ddef6f080068b518575288598baa909556c0
Vu.TranNguyenMinh <tnmvu93@gmail.com>
2016-11-10 17:19:57 +0700

    Config Effort, Writing 1st unit test function

---
+++ MovieRentalShop.Handler/Handlers/Users/LoginHandler.cs
@@ -0,0 +1,28 @@
+﻿using MovieRentalShop.Handler.Context;
+using MovieRentalShop.Handler.Queries.Users;
+using MovieRentalShop.Handler.QueryResponses.Users;
+using System.Linq;
+
+namespace MovieRentalShop.Handler.Handlers
+{
+    public class LoginHandler : IQueryHandler<LoginQuery, LoginQueryResponse>
+    {
+        private MovieRentalShopContext _dbContext;
+
+        public LoginHandler(MovieRentalShopContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        public LoginQueryResponse Handle(LoginQuery query)
+        {
+            var response = new LoginQueryResponse { Success = false };
+
+            if (query != null)
+            {
+                response.Success = _dbContext.Users.Any(x => x.Email == query.Email && x.Password == query.Password);
+            }
+            return response;
+        }
+    }
+}
