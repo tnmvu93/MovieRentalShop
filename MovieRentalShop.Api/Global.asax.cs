1767bb2ff6b60728b184ad82accfb162f17e3c73
Vu.TranNguyenMinh <tnmvu93@gmail.com>
2016-12-09 14:18:59 +0700

    - Set camel-case json.

--- MovieRentalShop.Api/Global.asax.cs
+++ MovieRentalShop.Api/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Http;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Web.Http;
 
 namespace MovieRentalShop.Api
 {
@@ -9,6 +11,17 @@ namespace MovieRentalShop.Api
             GlobalConfiguration.Configure(WebApiConfig.Register);
 
             IocContainer.SetUp();
+
+            ConfigureCamelCaseJson();
+        }
+
+        // Ref http://www.developerhandbook.com/c-sharp/create-restful-api-authentication-using-web-api-jwt/
+        private void ConfigureCamelCaseJson()
+        {
+            var jsonFormatter = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+            var settings = jsonFormatter.SerializerSettings;
+            settings.Formatting = Formatting.Indented;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
